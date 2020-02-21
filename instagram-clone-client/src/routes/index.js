20a6b85910bed6caab431bffa9b6257b23fd849c
3Shu <eshumiwa@gmail.com>
2020-02-21 16:37:58 -0600

    Formulario de login y de singup

--- instagram-clone-client/src/routes/index.js
+++ instagram-clone-client/src/routes/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
   } from "react-router-dom";
 import Home from './home'; 
-import Toolbar from '../components/toolbar'; 
-  
-  const Register = ()=> [<Toolbar/>,<h1>Register</h1>]
-  
-  export default () => (
-      <Router>
-          <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/register" exact component={Register} />
-          </Switch>
-      </Router>
-  )
+import Login from './login'; 
+import 'semantic-ui-css/semantic.min.css';  
+
+export default () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+        </Switch>
+    </Router>
+)
   
---
+++ instagram-clone-client/src/routes/login/Signin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Divider, Form, Button, Icon } from 'semantic-ui-react'
+
+
+export default ({styles, handleClick, handleSubmit}) => {
+  const args={}
+
+  const handleChange = (ev, input)=>{
+    args[input.name] = input.value
+  }
+
+  return (
+  <div>
+    <div  style={styles.box}>
+        <h2>Instagram-Clone</h2>
+        <Form onSubmit={(ev)=>handleSubmit(ev, args)}>
+          <Form.Field>
+             <Form.Input name="email_signin" onChange={handleChange} placeholder='email o nombre de usuario' icon={<Icon name="user" size="large" />} />
+           </Form.Field>
+           <Form.Field>
+             <Form.Input name="password_signin" onChange={handleChange} type="password" placeholder='Password' icon={<Icon name='eye slash' size="large" />} />
+           </Form.Field>
+           <Button type='submit' primary fluid>Iniciar sesión</Button>
+          <Divider horizontal> O </Divider>
+          <Button color="facebook">
+            <Icon name="facebook" /> Iniciar sesión con facebook
+          </Button>
+        </Form>
+    </div>
+    <div  style={styles.box}>
+        ¿No tienes una cuenta? <a href="/" onClick={handleClick}>Regístrate</a>
+    </div>
+  </div>)
+}
